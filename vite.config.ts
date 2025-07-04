96be7307b72f3d3b091ddce5c04de9e3dabe6dd1
Parul Bhatnagar <parulbhatnagar@users.noreply.github.com>
2025-07-04 15:00:52 +0530

    Update vite.config.ts removing dependency to enter api key

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
