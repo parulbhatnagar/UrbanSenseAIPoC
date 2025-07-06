633d5d76056e3a8df7804ed5f26c52cc688d630f
Parul Bhatnagar <parulbhatnagar@users.noreply.github.com>
2025-07-06 16:46:33 +0530

    Update ApiKeyModal.tsx

--- components/ApiKeyModal.tsx
+++ components/ApiKeyModal.tsx
@@ -1,9 +1,1 @@
 
-// This component is deprecated and no longer in use.
-// The API key has been moved to a hardcoded constant in `services/geminiService.ts`
-// for simplicity during development. This file can be safely deleted.
-import React from 'react';
-
-const ApiKeyModal: React.FC = () => null;
-
-export default ApiKeyModal;
